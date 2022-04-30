b2ffda339e8a4631a8847e30151ab3359c49913d
Fristtram Helder Fernandes <fristtram@gmail.com>
2022-04-30 16:22:37 -0300

    Cadastro com email e senha, cadastro anonimo e login com tratamento de erros

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
@@ -6,6 +8,45 @@ import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignInAnonymously() {
+    const { user } = await auth().signInAnonymously();
+  }
+
+  function handleCreateUserAccount() {
+    auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      Alert.alert('Usuário criado com sucesso!');
+    })
+    .catch(error => {
+      console.log(error.code);
+      if (error.code === 'auth/email-already-in-use') {
+        return Alert.alert('E-mail já em uso. cadastre um novo e-mail!')
+      }
+      if (error.code === 'auth/weak-password') {
+        return Alert.alert('A senha deve ter no mínimo 6 digitos.')
+      }
+      if (error.code === 'auth/invalid-email') {
+        return Alert.alert('E-mail inválido!')
+      }
+    });
+  }
+
+  function handleSignInWithEmailAndPassword(){
+    auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(({ user }) => {
+    })
+    .catch(error => {
+      if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password') {
+       return Alert.alert('Usuário não encontrada. E-mail e/ou senha inválida!');
+      }
+    });
+  }
+
   return (
     <Container>
       <Title>Lista de Compra</Title>
@@ -14,18 +55,20 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
 
       <Input
         placeholder="senha"
         secureTextEntry
+        onChangeText={setPassword}
       />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={handleSignInWithEmailAndPassword} />
 
       <Account>
         <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Criar minha conta" onPress={handleCreateUserAccount} />
       </Account>
     </Container>
   );
